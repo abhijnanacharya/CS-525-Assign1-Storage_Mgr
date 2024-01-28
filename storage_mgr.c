4b7600175f5613095dbc66feb4f03c9369ccd6dc
abhijnanacharya <abhijnanacharya11@gmail.com>
2024-01-27 21:22:09 -0600

    Create page Function Added

---
+++ storage_mgr.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "storage_mgr.h"
+#include "dberror.h"
+
+
+FILE *fp = NULL;
+
+extern void initStorageManager()
+{
+  printf("**********Storage Manager V0.0.1 initialized**********\n");
+}
+// Creation of the page file
+extern RC createPageFile(char *fName)
+{
+  RC rc = RC_OK;
+
+  // If the file already exists, change it to empty.
+  openFile(fp, fName, "wb+");
+
+  // If file pointer is NULL, return File Not Found Error.
+  if (fp == NULL)
+  {
+    return RC_FILE_NOT_FOUND;
+  }
+
+  // Allocating memory to PAGE_SIZE element
+  SM_PageHandle blankPage = (SM_PageHandle)calloc(PAGE_SIZE, sizeof(char));
+
+  if (blankPage == NULL)
+  {
+    // Handle memory allocation failure.
+    fclose(fp);
+    return RC_MEM_ALLOC_FAILED;
+  }
+
+  // Writing an empty page to the file
+  size_t wPStatus = fwrite(blankPage, sizeof(char), PAGE_SIZE, fp);
+
+  if (wPStatus == 0)
+  {
+    rc = RC_WRITE_FAILED;
+  }
+  else
+  {
+    rc = RC_OK;
+  }
+
+  // free memory to avoid memory leakage
+  free(blankPage);
+
+  // Closing the file
+  int cFile = fclose(fp);
+
+  if (cFile == EOF)
+  {
+    // Handle file closure failure.
+    rc = RC_CLOSE_FILE_FAILED;
+  }
+
+  return rc;
+}
