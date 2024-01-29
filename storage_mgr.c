0ca4c52dbf36f8fd918c74a072c571c3e9ce67b2
abhijnanacharya <abhijnanacharya11@gmail.com>
2024-01-29 10:44:45 -0600

    openPageFile functionality Added

--- storage_mgr.c
+++ storage_mgr.c
@@ -3,7 +3,6 @@
 #include "storage_mgr.h"
 #include "dberror.h"
 
-
 FILE *fp = NULL;
 
 extern void initStorageManager()
@@ -13,7 +12,7 @@ extern void initStorageManager()
 // Creation of the page file
 extern RC createPageFile(char *fName)
 {
-  RC rc = RC_OK;
+  RC returnCode = RC_OK;
 
   // If the file already exists, change it to empty.
   openFile(fp, fName, "wb+");
@@ -39,11 +38,11 @@ extern RC createPageFile(char *fName)
 
   if (wPStatus == 0)
   {
-    rc = RC_WRITE_FAILED;
+    returnCode = RC_WRITE_FAILED;
   }
   else
   {
-    rc = RC_OK;
+    returnCode = RC_OK;
   }
 
   // free memory to avoid memory leakage
@@ -55,8 +54,32 @@ extern RC createPageFile(char *fName)
   if (cFile == EOF)
   {
     // Handle file closure failure.
-    rc = RC_CLOSE_FILE_FAILED;
+    returnCode = RC_CLOSE_FILE_FAILED;
   }
 
-  return rc;
+  return returnCode;
+}
+
+extern RC openPageFile(char *fileName, SM_FileHandle *fHandle)
+{
+
+  RC returnCode = RC_OK;
+  // Open Page for read and write purpose
+  openFile(fp, fileName, "r+");
+
+  if (!fp)
+    return returnCode = RC_FILE_NOT_FOUND;
+
+  // Initialize fields of fHandle
+  fHandle->fileName = fileName;
+  fHandle->totalNumPages = 0;
+  fHandle->curPagePos = 0;
+  fHandle->mgmtInfo = fp;
+
+  // Read the total number of pages from the file (if available)
+  fseek(fp, 0L, SEEK_END);
+  fHandle->totalNumPages = ftell(fp) / PAGE_SIZE;
+
+  // Return success code
+  return returnCode;
 }
