f473de860f44b2dda725a5f98c90c2a3a31e6700
abhijnanacharya <abhijnanacharya11@gmail.com>
2024-01-31 22:16:35 -0600

    DestroyPageFile Added

--- storage_mgr.c
+++ storage_mgr.c
@@ -3,10 +3,11 @@
 #include "storage_mgr.h"
 #include "dberror.h"
 
-FILE *fp = NULL;
+FILE *fp;
 
 extern void initStorageManager()
 {
+  fp = NULL;
   printf("**********Storage Manager V0.0.1 initialized**********\n");
 }
 // Creation of the page file
@@ -20,24 +21,33 @@ extern RC createPageFile(char *fName)
   // If file pointer is NULL, return File Not Found Error.
   if (fp == NULL)
   {
-    return RC_FILE_NOT_FOUND;
+    returnCode = RC_FILE_NOT_FOUND;
+    RC_message = "ERR: UNABLE TO LOCATE FILE";
+    printError(*RC_message);
+    return returnCode;
   }
-
+  size_t blockNum = PAGE_SIZE;
+  size_t blockSize = sizeof(char);
   // Allocating memory to PAGE_SIZE element
-  SM_PageHandle blankPage = (SM_PageHandle)calloc(PAGE_SIZE, sizeof(char));
+  SM_PageHandle newblankPage = (SM_PageHandle)calloc(blockNum,blockSize);
 
-  if (blankPage == NULL)
+  if (newblankPage == NULL)
   {
     // Handle memory allocation failure.
     fclose(fp);
-    return RC_MEM_ALLOC_FAILED;
+    returnCode = RC_MEM_ALLOC_FAILED;
+    RC_message = "ERR: MEMORY ALLOCATION FAILURE";
+    printError(*RC_message);
+    return returnCode;
   }
 
   // Writing an empty page to the file
-  size_t wPStatus = fwrite(blankPage, sizeof(char), PAGE_SIZE, fp);
+  size_t writeEmptyPageStatus = fwrite(newblankPage, blockSize, blockNum, fp);
 
-  if (wPStatus == 0)
+  if (writeEmptyPageStatus == 0)
   {
+    RC_message = "ERR: FAILED TO WRITE ON PAGE";
+    printError(*RC_message);
     returnCode = RC_WRITE_FAILED;
   }
   else
@@ -46,7 +56,7 @@ extern RC createPageFile(char *fName)
   }
 
   // free memory to avoid memory leakage
-  free(blankPage);
+  free(newblankPage);
 
   // Closing the file
   int cFile = fclose(fp);
@@ -54,6 +64,8 @@ extern RC createPageFile(char *fName)
   if (cFile == EOF)
   {
     // Handle file closure failure.
+    RC_message = "ERR: FAILED TO CLOSE FILE";
+    printError(*RC_message);
     returnCode = RC_CLOSE_FILE_FAILED;
   }
 
@@ -68,7 +80,11 @@ extern RC openPageFile(char *fileName, SM_FileHandle *fHandle)
   openFile(fp, fileName, "r+");
 
   if (!fp)
+  {
+    RC_message = "ERR: FILE NOT FOUND";
+    printError(*RC_message);
     return returnCode = RC_FILE_NOT_FOUND;
+  }
 
   // Initialize fields of fHandle
   fHandle->fileName = fileName;
@@ -84,30 +100,64 @@ extern RC openPageFile(char *fileName, SM_FileHandle *fHandle)
   return returnCode;
 }
 
-extern RC closePageFile(SM_FileHandle *fHandle){
-// Check if the file handle is NULL or if the file is not initialized
-    openFile(fp, fHandle->fileName, "r+");
-    printf("************Closing Page File****************\n");
-    int returnCode = RC_OK;
-    if (fHandle == NULL || !fp) {
-        returnCode = RC_FILE_NOT_FOUND;
-        RC_message = "ERR: FILE DOES NOT EXIST!";
-        printError(*RC_message);
-        return returnCode;
-    }
+extern RC closePageFile(SM_FileHandle *fHandle)
+{
+  // Check if the file handle is NULL or if the file is not initialized
+  openFile(fp, fHandle->fileName, "r+");
+  printf("************CLOSING PAGE FILE****************\n");
+  int returnCode = RC_OK;
+  if (fHandle == NULL || !fp)
+  {
+    returnCode = RC_FILE_NOT_FOUND;
+    RC_message = "ERR: FILE DOES NOT EXIST!";
+    printError(*RC_message);
+    return returnCode;
+  }
 
-    // Close the file
-    int closeStatus= fclose(fHandle->mgmtInfo);
+  // Close the file
+  int closeStatus = fclose(fHandle->mgmtInfo);
 
-    if(closeStatus==0){
-      returnCode = RC_OK;
-      RC_message = "CLOSED SUCCESFULLY";
-      printError(*RC_message);
-      return returnCode;
-    }else{
-      returnCode = RC_CLOSE_FILE_FAILED;
-      RC_message = "ERR: UNABLE TO CLOSE FILE";
-      printError(*RC_message);
-      return returnCode;
-    }
+  if (closeStatus == 0)
+  {
+    returnCode = RC_OK;
+    RC_message = "CLOSED SUCCESSFULLY";
+    printError(*RC_message);
+    return returnCode;
+  }
+  else
+  {
+    returnCode = RC_CLOSE_FILE_FAILED;
+    RC_message = "ERR: UNABLE TO CLOSE FILE";
+    printError(*RC_message);
+    return returnCode;
+  }
+}
+
+extern RC destroyPageFile(char *fileName)
+{
+  int returnCode = RC_OK;
+  // Close the file if it is Open
+  if (fp != NULL)
+  {
+    fclose(fp);
+    fp = NULL;
+  }
+  // Delete the File
+  int destroyPageFileStatus = remove(fileName);
+
+  // Check if success, return Appropriate Return codes
+  if (destroyPageFileStatus == 0)
+  {
+    returnCode = RC_OK;
+    RC_message = "DESTROYED SUCCESFULLY";
+    printError(*RC_message);
+    return returnCode;
+  }
+  else
+  {
+    returnCode = RC_FILE_NOT_FOUND;
+    RC_message = "ERR: FAILED TO DESTROY PAGE FILE!!";
+    printError(*RC_message);
+    return returnCode;
+  }
 }
